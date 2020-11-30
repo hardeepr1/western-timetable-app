bf026179c92e0193601a2602139381f30e34d5f8
Hardeep Raike <hraike@uwo.ca>
2020-11-30 10:07:06 -0500

    Changing the folder structure

--- models/Course.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-
-//course can have different schema
-const model = new Schema({}, { strict: false });
-
-module.exports = mongoose.model('Course', model);
