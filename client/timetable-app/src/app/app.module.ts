a63e4f8347a8a1a0eb635762529b82b3a1bacf3e
Hardeep Raike <hraike@uwo.ca>
2020-11-30 11:45:51 -0500

    Adding angular material for styling and register component

--- client/timetable-app/src/app/app.module.ts
+++ client/timetable-app/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//need to move into it own module
+import {MatButtonModule} from  '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
